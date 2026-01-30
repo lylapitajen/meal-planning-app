f31df64be92e997879d82ff3c108fdb8534a15db
lylapitajen <lylapitajen00@gmail.com>
2026-01-30 19:41:02 +0000

    installed shadcn-svelte package

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,22 @@
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 import type { Ingredient } from './types';
 import { PUBLIC_STRAPI_URL } from '$env/static/public';
 
+// SVELTE-SHADCN/TW UTILS
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type WithoutChild<T> = T extends { child?: any } ? Omit<T, 'child'> : T;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type WithoutChildren<T> = T extends { children?: any } ? Omit<T, 'children'> : T;
+export type WithoutChildrenOrChild<T> = WithoutChildren<WithoutChild<T>>;
+export type WithElementRef<T, U extends HTMLElement = HTMLElement> = T & { ref?: U | null };
+
+//CUSTOM UTILS
 export const displayIngredientsInfo = (ingredients: Ingredient[], count: boolean = true): string => {
   if (count) {
     return `${ingredients.length} ingredient${ingredients.length > 1 ? 's' : ''}`;
